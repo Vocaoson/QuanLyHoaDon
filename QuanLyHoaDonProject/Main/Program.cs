5b2d759160cc10e5dda433711a8132acf6e251b7
VoCaoSon <vocaoson7@gmail.com>
2018-05-19 10:54:38 +0700

    Final

--- QuanLyHoaDonProject/Main/Program.cs
+++ QuanLyHoaDonProject/Main/Program.cs
@@ -22,7 +22,7 @@ namespace Main
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
+            Application.Run(new frmStartProgram());
         }
     }
 }
